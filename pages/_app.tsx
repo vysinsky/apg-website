5b27afd5480dfed6518f7426f75a4b2c6befeedc
Michal Vyšinský <michal@vysinsky.cz>
2021-08-21 21:33:42 +0200

    feat: favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,6 +39,26 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Layout>
         <Component {...pageProps} />
