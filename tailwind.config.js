df51c856eda82cb042fe2ed0e658de3b5eba430b
Michal Vyšinský <michal@vysinsky.cz>
2024-12-28 21:19:41 +0100

    fix: tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'media',
   theme: {
     extend: {
       colors: {
